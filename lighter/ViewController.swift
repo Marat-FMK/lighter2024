8b5b3f8639b084229805cc748c668647c61b3121
MaratXcode <kosjachok8@yandex.ru>
2024-05-30 22:11:59 +0300

    Initial Commit

---
+++ lighter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lighter
+//
+//  Created by Marat Fakhrizhanov on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
