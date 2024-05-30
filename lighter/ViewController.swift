48938383c4ac6cb0015135464baba1c85e0b73c4
MaratXcode <kosjachok8@yandex.ru>
2024-05-30 22:42:01 +0300

    rename red const , work with logic and constraints

--- lighter/ViewController.swift
+++ lighter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    
+    @IBOutlet var startButtonText: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
